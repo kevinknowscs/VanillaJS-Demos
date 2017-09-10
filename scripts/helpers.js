9d9db98d3da4336b810385804d95da678f892903
Kevin Dietz <kevinknowscs@gmail.com>
2017-09-10 12:05:41 -0600

    Various updates

---
+++ scripts/helpers.js
@@ -0,0 +1,15 @@
+function onDocumentReady(callback) {
+  if (document.readyState !== 'loading') {
+    callback();
+  }
+  else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', callback)
+  }
+  else if (document.attachEvent) {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState === 'complete') {
+        callback();
+      }
+    });
+  }
+}
