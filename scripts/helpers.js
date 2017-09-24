b7263ed8ac3bc0fad44137a6bea54cf6ccf7927d
Kevin Dietz <kevinknowscs@gmail.com>
2017-09-24 10:11:02 -0600

    CSS Layout - Media Queries

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -13,3 +13,25 @@ function onDocumentReady(callback) {
     });
   }
 }
+
+function addClass(el, className) {
+  if (el.classList) {
+    el.classList.add(className);
+  }
+  else if (!hasClass(el, className)) {
+    el.className += ' ' + className;
+  }
+}
+
+function removeClass(el, className) {
+  if (el.classList) {
+    el.classList.remove(className);
+  }
+  else {
+    el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+  }
+}
+
+function hasClass(el, className) {
+  return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+}
